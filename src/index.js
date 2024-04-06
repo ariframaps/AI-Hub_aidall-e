9c787bcbb9db4097b3f6a41eff68881497968576
ariframaps <ariframa@student.ub.ac.id>
2024-04-06 09:49:36 +0700

    membuat animasi paralax pada gambar di section hero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+// hero-mouse tracker
 document.addEventListener("mousemove", function (event) {
     var follower = document.querySelector(".bi-triangle-fill");
     var mouseX = event.clientX;
@@ -15,4 +17,28 @@ document.addEventListener("mousemove", function (event) {
     // Mengatur transformasi rotasi pada elemen pengikut
     follower.style.transform = "rotate(" + angle + "deg)";
 });
+
+//hero-rideimage animation
+let scrollY = 0;
+const heroImg_l1 = document.querySelector('.l1');
+const heroImg_r1 = document.querySelector('.r1');
+let l1Position = 100;
+let r1Position = 0;
+
+document.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > scrollY) {
+        l1Position -= 3
+        r1Position += 3
+        heroImg_l1.style.transform = `translateY(${l1Position}px)`
+        heroImg_r1.style.transform = `translateY(${r1Position}px)`
+    } else {
+        l1Position += 3
+        r1Position -= 3
+        heroImg_l1.style.transform = `translateY(${l1Position}px)`
+        heroImg_r1.style.transform = `translateY(${r1Position}px)`
+    }
+    scrollY = currentScroll;
+})
 
